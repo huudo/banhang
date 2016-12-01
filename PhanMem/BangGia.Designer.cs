fb8867dee2b07895f4611ec41605c3d5d14cb0c9
Trong Huu <huudt.3012@gmail.com>
2016-12-01 15:02:46 +0700

    isdflkas

--- PhanMem/BangGia.Designer.cs
+++ PhanMem/BangGia.Designer.cs
@@ -32,6 +32,9 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnImportDB = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -75,12 +78,44 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnImportDB);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(929, 232);
             this.panel1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(116, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 26);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bảng Giá ";
+            // 
+            // btnImportDB
+            // 
+            this.btnImportDB.Location = new System.Drawing.Point(817, 87);
+            this.btnImportDB.Name = "btnImportDB";
+            this.btnImportDB.Size = new System.Drawing.Size(92, 23);
+            this.btnImportDB.TabIndex = 1;
+            this.btnImportDB.Text = "Import DATA";
+            this.btnImportDB.UseVisualStyleBackColor = true;
+            this.btnImportDB.Click += new System.EventHandler(this.btnImportDB_Click);
             // 
             // button1
             // 
@@ -100,10 +135,12 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "BangGia";
             this.Text = "BangGia";
+            this.Load += new System.EventHandler(this.BangGia_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -115,5 +152,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnImportDB;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- PhanMem/BangGia.cs
+++ PhanMem/BangGia.cs
@@ -8,12 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Data.SqlClient;
 using Excel;
 
 namespace PhanMem
 {
     public partial class BangGia : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(Localdb)\v11.0;Integrated Security=True;AttachDbFilename=" + AppDomain.CurrentDomain.BaseDirectory + "quanly.mdf");
         public BangGia()
         {
             InitializeComponent();
@@ -21,7 +23,7 @@ namespace PhanMem
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog ofd = new OpenFileDialog() {Filter="Exel WorkBook|*.xlsx", ValidateNames = true })
+            using (OpenFileDialog ofd = new OpenFileDialog() {Filter="Exel WorkBook|*.xls || *.xlsx", ValidateNames = true })
             {
                 if (ofd.ShowDialog() == DialogResult.OK)
                 {
@@ -34,6 +36,111 @@ namespace PhanMem
                     reader.Close();
                 }
             }
+        }
+
+        private void btnImportDB_Click(object sender, EventArgs e)
+        {
+            int id_bangGia = 0;
+            DateTime dateNow = DateTime.Now;
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+            }
+            
+            SqlCommand cmd = new SqlCommand();
+            String query = "select max(id) from banggia";
+            cmd.Connection = con;
+            cmd.CommandText = query;
+            try
+            {
+                id_bangGia = Convert.ToInt32(cmd.ExecuteScalar()) + 1;
+            }
+            catch (Exception ex)
+            {
+                id_bangGia = 1;
+            }
+            String querryAdd = "INSERT INTO banggia(date,status) VALUES(@date,@status)";
+            var cmdAdd = new SqlCommand(querryAdd, con);
+
+            cmdAdd.Parameters.AddWithValue("@date", dateNow);
+            cmdAdd.Parameters.AddWithValue("@status", 0);
+            cmdAdd.ExecuteNonQuery();
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                var stringQr = "INSERT INTO sanpham (mahang,name,donvi,khoiluong,giado,ck1,ck2,ck3,gianet,id_banggia)" +
+               "VALUES(@mahang,@name,@donvi,@khoiluong,@giado,@ck1,@ck2,@ck3,@gianet,@id_banggia)";
+                var cmdRun = new SqlCommand(stringQr, con);
+                cmdRun.Parameters.AddWithValue("@mahang", dataGridView1.Rows[i].Cells[1].Value.ToString());
+                cmdRun.Parameters.AddWithValue("@name",dataGridView1.Rows[i].Cells[2].Value.ToString());
+                cmdRun.Parameters.AddWithValue("@donvi", dataGridView1.Rows[i].Cells[3].Value.ToString());
+                cmdRun.Parameters.AddWithValue("@khoiluong", Int32.Parse(dataGridView1.Rows[i].Cells[4].Value.ToString()));
+                cmdRun.Parameters.AddWithValue("@giado", float.Parse(dataGridView1.Rows[i].Cells[5].Value.ToString()));
+                if (dataGridView1.Rows[i].Cells[6].Value.ToString() != "")
+                {
+                    cmdRun.Parameters.AddWithValue("@ck1", Int32.Parse(dataGridView1.Rows[i].Cells[6].Value.ToString()));
+                }
+                else
+                {
+                    cmdRun.Parameters.AddWithValue("@ck1", 0);
+                }
+                if (dataGridView1.Rows[i].Cells[7].Value.ToString() != "")
+                {
+                    cmdRun.Parameters.AddWithValue("@ck2", Int32.Parse(dataGridView1.Rows[i].Cells[7].Value.ToString()));
+                }
+                else
+                {
+                    cmdRun.Parameters.AddWithValue("@ck2", 0);
+                }
+                if (dataGridView1.Rows[i].Cells[8].Value.ToString() != "")
+                {
+                    cmdRun.Parameters.AddWithValue("@ck3", Int32.Parse(dataGridView1.Rows[i].Cells[8].Value.ToString()));
+                }
+                else
+                {
+                    cmdRun.Parameters.AddWithValue("@ck3", 0);
+                }
+                cmdRun.Parameters.AddWithValue("@gianet", float.Parse(dataGridView1.Rows[i].Cells[9].Value.ToString()));
+                cmdRun.Parameters.AddWithValue("@id_banggia", id_bangGia);
+                 
+                cmdRun.ExecuteNonQuery();               
+            }
+            MessageBox.Show("ad");
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+        public class Gia
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            Gia obj = comboBox1.SelectedItem as Gia;
+            if (obj != null)
+            {
+                MessageBox.Show(obj.id.ToString());
+            }
+        }
+
+        private void BangGia_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            List<Gia> list = new List<Gia>();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT id,date FROM banggia", con);
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                list.Add(new Gia() { id = Int32.Parse(dr["id"].ToString()), name = dr["date"].ToString() });
+            }
+            dr.Close();
+            comboBox1.DataSource = list;
+            comboBox1.ValueMember = "id";
+            comboBox1.DisplayMember = "name";
         }
     }
 }
